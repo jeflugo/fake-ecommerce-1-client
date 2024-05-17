969cac735595bfb9bf0ec2bcd26e19ab07388cb5
jeflugo <jefersoneduardoluego@gmail.com>
2024-05-16 22:43:19 -0400

    hore section on tablets done

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,10 +1,13 @@
 import { Button } from '@material-tailwind/react'
-import { HERO_BANNER } from '../../constants'
+import { HERO_BANNERS } from '../../constants'
 
 function HeroSection() {
+	const { SM, MD, LG } = HERO_BANNERS
 	return (
-		<div className='relative'>
-			<img src={HERO_BANNER} className='w-full' />
+		<div className='relative lg:bg-gray-50'>
+			<img src={SM} className='w-full md:hidden' />
+			<img src={MD} className='hidden w-full md:block lg:hidden' />
+			<img src={LG} className='hidden w-full lg:block' />
 			<div className='absolute bottom-0  w-full   pb-10'>
 				<div className='mx-auto w-9/12 max-w-[420px]'>
 					<h1 className='mb-6 text-center text-4xl font-medium underline'>
