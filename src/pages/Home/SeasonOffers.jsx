a62c6189dd4d2e8deb0c6015af4ce7629a730744
jeflugo <jefersoneduardoluego@gmail.com>
2024-05-19 22:42:36 -0400

    Carousel done

--- src/pages/Home/SeasonOffers.jsx
+++ src/pages/Home/SeasonOffers.jsx
@@ -4,10 +4,11 @@ import { SEASON_OFFERS, SEASON_OFFERS_IMGS } from '../../constants'
 import { BiChevronLeft, BiChevronRight } from 'react-icons/bi'
 import { useEffect, useState } from 'react'
 
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion, useAnimate } from 'framer-motion'
 
 function SeasonOffers() {
 	const [active, setActive] = useState(1)
+	const [scope, animate] = useAnimate()
 
 	const [imgs, setImgs] = useState()
 
@@ -52,28 +53,28 @@ function SeasonOffers() {
 					})}
 				</div>
 				<div className='hidden w-full justify-center py-4 md:flex'>
-					<div className='flex items-center'>
+					<div className='flex items-center' ref={scope}>
 						{imgs && (
 							<>
 								{imgs.prev ? (
 									<motion.img
 										src={imgs.prev}
-										className='h-[200px] w-[150px] rounded'
+										className='box h-[200px] w-[150px] rounded '
 									/>
 								) : (
-									<div className='h-[200px] w-[150px] rounded' />
+									<motion.img className='box invisible h-[200px] w-[150px]' />
 								)}
 								<motion.img
 									src={imgs.curr}
-									className='h-[400px] w-[300px] rounded'
+									className='box h-[400px] w-[300px] rounded '
 								/>
 								{imgs.next ? (
 									<motion.img
 										src={imgs.next}
-										className='h-[200px] w-[150px] rounded'
+										className='box h-[200px] w-[150px] rounded '
 									/>
 								) : (
-									<div className='h-[200px] w-[150px] rounded' />
+									<motion.img className='box invisible h-[200px] w-[150px]' />
 								)}
 							</>
 						)}
@@ -89,7 +90,13 @@ function SeasonOffers() {
 						className='disabled:opacity-30'
 					>
 						<BiChevronLeft
-							onClick={backCarousel}
+							onClick={() => {
+								animate('.box', {
+									opacity: [0, 1],
+									scale: [0.3, 1],
+								})
+								backCarousel()
+							}}
 							className={`h-9 w-9 rounded-full bg-gray-500 opacity-70`}
 						/>
 					</button>
@@ -100,7 +107,13 @@ function SeasonOffers() {
 						className='disabled:opacity-30'
 					>
 						<BiChevronRight
-							onClick={fowardCarousel}
+							onClick={() => {
+								animate('.box', {
+									opacity: [0, 1],
+									scale: [0.3, 1],
+								})
+								fowardCarousel()
+							}}
 							className={`h-9 w-9 rounded-full bg-gray-500 opacity-70`}
 						/>
 					</button>
