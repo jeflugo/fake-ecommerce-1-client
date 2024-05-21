5cd20687c255a799dfec94078fac37a38bf6a0f1
jeflugo <jefersoneduardoluego@gmail.com>
2024-05-20 21:57:29 -0400

    fetching products from sanity data done

--- src/context/StateContext.jsx
+++ src/context/StateContext.jsx
@@ -1,12 +1,19 @@
-import { createContext, useContext } from 'react'
+import { useState, useEffect, createContext, useContext } from 'react'
+import { client } from '../lib/client'
 
 const Context = createContext()
 
 export default function StateContext({ children }) {
-	const getProducts = () => {
-		console.log('first')
-	}
-	return <Context.Provider value={{ getProducts }}>{children}</Context.Provider>
+	const [products, setProducts] = useState()
+
+	useEffect(() => {
+		const productsQuery = '*[_type=="product"]'
+
+		client.fetch(productsQuery).then(data => {
+			setProducts(data)
+		})
+	}, [])
+	return <Context.Provider value={{ products }}>{children}</Context.Provider>
 }
 
 export const useStateContext = () => useContext(Context)
