bf11357eb997606eb908f62ea3942efd58b87fa7
jeflugo <jefersoneduardoluego@gmail.com>
2024-05-20 21:58:04 -0400

    initial carousel for best sellers

--- src/pages/Home/BestSellers.jsx
+++ src/pages/Home/BestSellers.jsx
@@ -1,9 +1,19 @@
 import { Link } from 'react-router-dom'
 import Container from '../../components/Container'
-import { BEST_SELLERS } from '../../constants'
 import { BiChevronLeft, BiChevronRight } from 'react-icons/bi'
+import { useStateContext } from '../../context/StateContext'
+import { useState, useEffect } from 'react'
+import { urlFor } from '../../lib/client'
+import { Carousel } from '@material-tailwind/react'
 
 function BestSellers() {
+	const [bestSellers, setBestSellers] = useState()
+	const { products } = useStateContext()
+
+	useEffect(() => {
+		if (products) setBestSellers(products)
+	}, [products])
+
 	return (
 		<div>
 			<div className='py-4'>
@@ -13,26 +23,42 @@ function BestSellers() {
 					</div>
 				</Container>
 			</div>
-			<div className='relative'>
-				<div>
-					{BEST_SELLERS.map(({ img, name, url }, index) => (
+			<Carousel
+				prevArrow={({ handlePrev, activeIndex }) => (
+					<button
+						disabled={activeIndex === 0 ? true : false}
+						className='absolute top-0 ml-6 flex h-full items-center disabled:opacity-30'
+					>
+						<BiChevronLeft
+							className='h-9 w-9 rounded-full bg-gray-500 opacity-70'
+							onClick={handlePrev}
+						/>
+					</button>
+				)}
+				nextArrow={({ handleNext, activeIndex }) => (
+					<button
+						disabled={activeIndex === bestSellers.length - 1 ? true : false}
+						className='absolute right-0 top-0 mr-6 flex h-full items-center disabled:opacity-30'
+					>
+						<BiChevronRight
+							className='h-9 w-9 rounded-full bg-gray-500 opacity-70'
+							onClick={handleNext}
+						/>
+					</button>
+				)}
+			>
+				{bestSellers &&
+					bestSellers.map(({ images, name, slug }, index) => (
 						<div key={index} className='relative'>
-							<Link to={url}>
-								<img src={img} className='w-full' />
+							<Link to={slug}>
+								<img src={urlFor(images[0])} className='w-full' />
 							</Link>
 							<h3 className='absolute bottom-6 w-full text-center text-xl font-medium'>
 								{name}
 							</h3>
 						</div>
 					))}
-				</div>
-				<div className='absolute top-0 flex h-full w-full items-center'>
-					<div className='mx-auto flex w-11/12 justify-between'>
-						<BiChevronLeft className='h-9 w-9 rounded-full bg-gray-500 opacity-70' />
-						<BiChevronRight className='h-9 w-9 rounded-full bg-gray-500 opacity-70' />
-					</div>
-				</div>
-			</div>
+			</Carousel>
 		</div>
 	)
 }
