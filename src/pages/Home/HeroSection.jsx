8d2e597d0abcd9c74a0b161edb1e428498c7081b
jeflugo <jefersoneduardoluego@gmail.com>
2024-05-22 15:16:55 -0400

    Hero done

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -6,18 +6,18 @@ function HeroSection() {
 	const { SM, MD, LG } = HERO_BANNERS
 	const { md, lg, width } = useStateContext()
 	return (
-		<div className='relative lg:bg-gray-50'>
+		<div className='relative lg:bg-gray-50 lg:py-6'>
 			{width < md && <img src={SM} className='w-full' />}
 			{width > md && width < lg && <img src={MD} className='w-full' />}
-			{width > lg && <img src={LG} className='w-full' />}
+			{width > lg && <img src={LG} className='ml-10 w-1/2' />}
 
-			<div className='absolute bottom-0 w-full pb-10'>
-				<div className='mx-auto w-9/12 max-w-[420px]'>
-					<h1 className='mb-6 text-center text-4xl font-medium underline'>
+			<div className='absolute lg:right-6 lg:top-0 lg:flex lg:h-full lg:items-center lg-max:bottom-0 lg-max:w-full lg-max:pb-10'>
+				<div className='mx-auto w-9/12 max-w-[420px] lg:mx-0'>
+					<h1 className='mb-6 text-center text-4xl font-medium underline lg:text-left'>
 						NEW Air Force Max Storm
 					</h1>
-					<div className='flex justify-center gap-4'>
-						<Button className='bg-black'>Buy now</Button>
+					<div className='flex justify-center gap-4 lg:justify-start'>
+						<Button size='lg'>Buy now</Button>
 						<div>
 							<p className='text-red-600 line-through'>$400</p>
 							<p className='text-green-600'>$200 (50%off)</p>
