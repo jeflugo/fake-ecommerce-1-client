376da3ae454a44a00cef98f2bc1cb3e011fa7e6d
jeflugo <jefersoneduardoluego@gmail.com>
2024-05-25 09:31:00 -0400

    Litle bug fixed

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,3 +1,5 @@
 export default function Container({ children, cName }) {
-	return <div className={`mx-auto w-10/12 py-3 ${cName}`}>{children}</div>
+	return (
+		<div className={`mx-auto w-10/12 py-3 ${cName && cName}`}>{children}</div>
+	)
 }
