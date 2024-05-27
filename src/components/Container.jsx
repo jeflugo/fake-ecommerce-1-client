2f2218423e30de54f2bf2e15d639c2a3fff73ad4
jeflugo <jefersoneduardoluego@gmail.com>
2024-05-27 14:34:54 -0400

    Container style modified

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,3 @@
 export default function Container({ children, cName }) {
-	return (
-		<div className={`mx-auto w-10/12 py-3 ${cName && cName}`}>{children}</div>
-	)
+	return <div className={`mx-auto w-10/12 ${cName && cName}`}>{children}</div>
 }
