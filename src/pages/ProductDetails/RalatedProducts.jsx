0cbd543f255534e365ecf15967d759b4aceda083
jeflugo <jefersoneduardoluego@gmail.com>
2024-05-27 14:04:52 -0400

    Product page design done

---
+++ src/pages/ProductDetails/RalatedProducts.jsx
@@ -0,0 +1,76 @@
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { urlFor } from '../../lib/client'
+import { BiHeart, BiPlus } from 'react-icons/bi'
+import { useStateContext } from '../../context/StateContext'
+
+function RalatedProducts({ currentId }) {
+	const [visibleOverlay, setVisibleOverlay] = useState()
+	const seeOverlay = index => setVisibleOverlay(index)
+	const { products } = useStateContext()
+	return (
+		<div>
+			{products && (
+				<>
+					<h3 className='mb-4 ml-2 text-xl font-medium'>Ralated products</h3>
+					<div className='relative flex h-[300px] justify-end'>
+						<div className='absolute left-0 top-0 z-10 h-full w-8 bg-gradient-to-r  from-gray-500 md:w-16' />
+						<div className='absolute right-0 top-0 z-10 h-full w-8 bg-gradient-to-l  from-gray-500 md:w-16' />
+						<div className='hide-scrollbar h-[100vw] w-[300px] origin-top-right translate-y-[300px] rotate-90 overflow-y-scroll scroll-smooth'>
+							{products.map(({ images, name, slug, price, _id }, index) => {
+								if (_id !== currentId) {
+									return (
+										<Link
+											key={index}
+											className='relative block h-[300px] w-[300px] -rotate-90'
+											to={`/store/${slug.current}`}
+											onMouseOver={() => seeOverlay(index)}
+										>
+											{visibleOverlay === index && (
+												<motion.div
+													initial={{ opacity: 0 }}
+													animate={{ opacity: 0.5 }}
+													className='absolute left-0 top-0 z-10 flex h-full w-full select-none bg-black p-6 text-left text-white'
+												>
+													<div className='absolute right-0 top-0 z-30 flex self-end pr-6 pt-6'>
+														<div className='flex gap-2'>
+															<BiHeart
+																size={30}
+																className='transition-all hover:scale-125 hover:fill-red-600 active:scale-100'
+															/>
+															<BiPlus
+																size={30}
+																className='transition-all hover:scale-125 active:scale-100'
+															/>
+														</div>
+													</div>
+													<div className='flex w-full items-center justify-between self-end text-2xl font-medium'>
+														<p>{name}</p>
+														<p>${price}</p>
+													</div>
+												</motion.div>
+											)}
+											<img
+												key={index}
+												src={urlFor(images[0])}
+												className='h-full w-full'
+											/>
+											{visibleOverlay !== index && (
+												<h3 className='absolute bottom-6 z-10 w-full text-center text-xl font-medium'>
+													{name}
+												</h3>
+											)}
+										</Link>
+									)
+								}
+							})}
+						</div>
+					</div>
+				</>
+			)}
+		</div>
+	)
+}
+
+export default RalatedProducts
---
+++ src/pages/ProductDetails/index.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { urlFor } from '../../lib/client'
+import { Button } from '@material-tailwind/react'
+import { BiHeart, BiPlus } from 'react-icons/bi'
+import RalatedProducts from './RalatedProducts'
+
+function ProductDetails(product) {
+	const { name, price, images, slug,_id } = product
+	const [currentImage, setCurrentImage] = useState(images[0])
+
+	const changeImage = index => setCurrentImage(images[index])
+
+	return (
+		<div>
+			<div className='mx-auto max-w-96 py-6'>
+				<h2 className='ml-2 text-2xl font-medium'>{name}</h2>
+				<div>
+					<div className='relative'>
+						<img
+							src={urlFor(currentImage)}
+							alt={slug.current}
+							className='mb-2 h-96 w-full'
+						/>
+						<BiHeart
+							className='absolute right-5 top-5 transition-all hover:scale-125 hover:fill-red-600 active:scale-100'
+							size={23}
+						/>
+					</div>
+					<div className='mb-4 flex flex-wrap gap-2'>
+						{images.map((img, index) => (
+							<img
+								key={index}
+								src={urlFor(img)}
+								onMouseEnter={() => changeImage(index)}
+								className='w-12'
+							/>
+						))}
+					</div>
+					<div className='ml-2 flex items-center gap-2'>
+						<Button className='flex gap-2'>
+							<span>Add to cart</span>
+							<span>
+								<BiPlus />
+							</span>
+						</Button>
+						<Button className='flex gap-2' variant='outlined'>
+							Buy now
+						</Button>
+						<h4 className='text-lg text-green-600'>${price}</h4>
+					</div>
+				</div>
+			</div>
+			<RalatedProducts currentId={_id}/>
+		</div>
+	)
+}
+
+export default ProductDetails
