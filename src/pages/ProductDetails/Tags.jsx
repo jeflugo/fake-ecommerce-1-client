33ca3968eae5e841bfa7db426ffc6e860b07255b
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-02 13:16:42 -0400

    tags styling modified

--- src/pages/ProductDetails/Tags.jsx
+++ src/pages/ProductDetails/Tags.jsx
@@ -1,6 +1,6 @@
 export default function Tags({ tags }) {
 	return (
-		<div className='flex gap-1'>
+		<div className='absolute left-5 top-5 flex gap-1'>
 			{tags.map((name, index) => (
 				<span key={index} className='text-sm'>
 					#{name}
