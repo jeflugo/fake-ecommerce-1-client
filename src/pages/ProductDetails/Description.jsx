9a23b78ee48fdcd1d0c77e925732fb8c373a454c
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-02 13:15:48 -0400

    added generic description for every shoe

---
+++ src/pages/ProductDetails/Description.jsx
@@ -0,0 +1,12 @@
+export default function Description() {
+	return (
+		<div className='mb-2'>
+			<h3 className='mb-1 text-xl font-medium'>Description</h3>
+			<p>
+				Step into style with our versatile and effortlessly chic shoe. These
+				shoes are the perfect combination of comfort and sophistication, making
+				them a must-have for any fashion-forward individual.
+			</p>
+		</div>
+	)
+}
