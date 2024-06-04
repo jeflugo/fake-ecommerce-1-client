50c655542605b14e7c842557f3383a9ef169a026
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-03 22:34:44 -0400

    litle styling change

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -13,14 +13,14 @@ function HeroSection() {
 
 			<div className='absolute lg:right-6 lg:top-0 lg:flex lg:h-full lg:items-center lg-max:bottom-0 lg-max:w-full lg-max:pb-10'>
 				<div className='mx-auto w-9/12 max-w-[380px] lg:mx-0'>
-					<h1 className='mb-6 text-center text-4xl font-medium underline lg:text-left'>
-						NEW Air Force Max Storm
+					<h1 className='mb-6 text-center text-4xl font-medium  lg:text-left'>
+						<span className='underline'>NEW</span> Air Force Max Storm
 					</h1>
 					<div className='flex justify-center gap-4 lg:justify-start'>
 						<Button size='lg'>Buy now</Button>
 						<div>
 							<p className='text-red-600 line-through'>$400</p>
-							<p className='text-green-600'>$200 (50%off)</p>
+							<p className='text-green-600'>$200 (50 %off)</p>
 						</div>
 					</div>
 				</div>
