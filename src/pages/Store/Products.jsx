65618175af1fcaff58c68f2461fb9530a2e9d8f0
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-03 22:37:38 -0400

    OrderBy in progress

--- src/pages/Store/Products.jsx
+++ src/pages/Store/Products.jsx
@@ -1,14 +1,23 @@
-import { useStateContext } from '../../context/StateContext'
 import Product from './Product'
-function Products() {
-	const { products } = useStateContext()
+
+function Products({ products, orderByOption }) {
 	return (
 		<div className='grid w-full grid-cols-3 place-items-center gap-2'>
 			{products && (
 				<>
-					{products.map((product, index) => (
-						<Product key={index} {...product} />
-					))}
+					{products
+						.sort((a, b) => {
+							if (orderByOption === 'Newest') {
+								const firstDate = new Date(a._createdAt)
+								const secondDate = new Date(b._createdAt)
+								return firstDate - secondDate
+							}
+							if (orderByOption === 'Price up') return b.price - a.price
+							if (orderByOption === 'Price down') return a.price - b.price
+						})
+						.map((product, index) => (
+							<Product key={index} {...product} />
+						))}
 				</>
 			)}
 		</div>
