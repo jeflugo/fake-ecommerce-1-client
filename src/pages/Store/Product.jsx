343956de07127a952e0ac0736af39a41c33c301a
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-03 22:36:41 -0400

    simplified data to render in each product

--- src/pages/Store/Product.jsx
+++ src/pages/Store/Product.jsx
@@ -2,7 +2,7 @@ import { BiHeart, BiPlus } from 'react-icons/bi'
 import { urlFor } from '../../lib/client'
 import { Link } from 'react-router-dom'
 
-function Product({ name, price, images, slug }) {
+function Product({ name, price, images: img, slug }) {
 	return (
 		<div className='relative max-w-[360px] lg:max-w-sm'>
 			<div className='absolute right-0 top-0 flex justify-end px-6 pt-6'>
@@ -18,9 +18,9 @@ function Product({ name, price, images, slug }) {
 				</div>
 			</div>
 			<Link to={`/store/${slug.current}`}>
-				<img src={urlFor(images[0])} className='w-full' />
+				<img src={urlFor(img)} className='w-full' />
 			</Link>
-			<h3 className='absolute bottom-0 flex w-full items-center justify-between px-6 pb-6 text-lg font-medium'>
+			<h3 className='absolute bottom-0 flex w-full items-end justify-between px-6 pb-6 text-lg font-medium'>
 				<span>{name}</span>
 				<span>${price}</span>
 			</h3>
