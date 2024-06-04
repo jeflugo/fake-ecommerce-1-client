37434157746e70ac54bf12a8f68e5d1347ce181d
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-03 22:35:38 -0400

    Categories functionality added

--- src/pages/Store/Categories.jsx
+++ src/pages/Store/Categories.jsx
@@ -1,12 +1,11 @@
 import { FaChevronRight } from 'react-icons/fa'
 import { SEARCH_CATEGORIES } from '../../constants'
-import { capitalizeFirstLetter } from '../../utils'
 import {
 	Accordion,
 	AccordionBody,
 	AccordionHeader,
 } from '@material-tailwind/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useStateContext } from '../../context/StateContext'
 
 function Categories() {
@@ -15,9 +14,12 @@ function Categories() {
 	const handleMainOpen = () => setMainOpen(!mainOpen)
 	const handleOpen = curr =>
 		setOpen(prevOpen => (curr === prevOpen ? null : curr))
-	const setCategories = () => {}
+	const { width, lg, category, setCategory } = useStateContext()
 
-	const { width, lg } = useStateContext()
+	useEffect(() => {
+		if (category) setOpen(category.catIndex)
+	}, [category])
+
 	return (
 		<>
 			{width < lg && (
@@ -43,12 +45,12 @@ function Categories() {
 									onClick={() => handleOpen(index)}
 									className='text-md p-0 py-1'
 								>
-									<h3>{capitalizeFirstLetter(name)}</h3>
+									<h3 className='first-letter:uppercase'>{name}</h3>
 								</AccordionHeader>
 								<AccordionBody className='ml-3'>
 									<ul>
 										{subs.map(({ name, value }, index) => (
-											<li key={index} onClick={() => setCategories(name)}>
+											<li key={index} onClick={() => setCategory(name)}>
 												{value}
 											</li>
 										))}
@@ -64,19 +66,28 @@ function Categories() {
 					<div>
 						<h2 className='text-xl font-medium'>Categories</h2>
 					</div>
-					{SEARCH_CATEGORIES.map(({ name, subs }, index) => (
-						<Accordion key={index} open={open === index} className='ml-3'>
+					{SEARCH_CATEGORIES.map(({ name: mainCatName, subs }, catIndex) => (
+						<Accordion key={catIndex} open={open === catIndex} className='ml-3'>
 							<AccordionHeader
-								onClick={() => handleOpen(index)}
-								className='p-0 py-1 text-lg'
+								onClick={() => handleOpen(catIndex)}
+								className='p-0 py-1 text-lg font-medium text-black'
 							>
-								<h3>{capitalizeFirstLetter(name)}</h3>
+								<h3 className='first-letter:uppercase'>{mainCatName}</h3>
 							</AccordionHeader>
 							<AccordionBody className='ml-3'>
 								<ul>
-									{subs.map(({ name, value }, index) => (
-										<li key={index} onClick={() => setCategories(name)}>
-											{value}
+									{subs.map(({ name: subCatName, value }, index) => (
+										<li
+											key={index}
+											onClick={() =>
+												setCategory({ subCatName, mainCatName, catIndex })
+											}
+										>
+											<button
+												className={`${category?.subCatName === subCatName && category?.catIndex === catIndex && 'font-bold'} hover:underline`}
+											>
+												{value}
+											</button>
 										</li>
 									))}
 								</ul>
