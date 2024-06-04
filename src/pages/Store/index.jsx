42092f871dd4e65fa24381f1417b6c019bf5dfc8
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-03 22:37:09 -0400

    categories filters added

--- src/pages/Store/index.jsx
+++ src/pages/Store/index.jsx
@@ -6,8 +6,32 @@ import Categories from './Categories'
 import Products from './Products'
 import { useStateContext } from '../../context/StateContext'
 
+import { useState, useEffect } from 'react'
+import { client } from '../../lib/client'
+import { ORDER_BY_OPTIONS } from '../../constants'
+
 function Store() {
 	const { width, lg } = useStateContext()
+	const [shownProducts, setShownProducts] = useState()
+	const { category } = useStateContext()
+
+	const [orderByOption, setOrderByOption] = useState(ORDER_BY_OPTIONS[0])
+
+	useEffect(() => {
+		let filtersQuery
+		if (category) {
+			if (category.mainCatName !== 'all') {
+				filtersQuery = ` && tags match '${category.mainCatName}'`
+				if (category.subCatName !== 'all')
+					filtersQuery =
+						filtersQuery + ` && tags match '${category.subCatName}'`
+			}
+		}
+
+		const productsQuery = `*[_type=="product"${filtersQuery ? filtersQuery : ''}]{ name, price, images[0], slug, _createdAt }`
+
+		client.fetch(productsQuery).then(data => setShownProducts(data))
+	}, [category])
 
 	return (
 		<div>
@@ -16,19 +40,29 @@ function Store() {
 			</Helmet>
 			<div className='py-6'>
 				<Container>
-					<div className='lg:max-w-[300px] lg:ml-auto'>
+					<div className='lg:ml-auto lg:max-w-[300px]'>
 						<SearchBar />
-						<OrderBy />
+						<OrderBy
+							orderByOption={orderByOption}
+							setOrderByOption={setOrderByOption}
+						/>
 						{width < lg && <Categories />}
 					</div>
 				</Container>
-				{width < lg && <Products />}
+				{width < lg && shownProducts && (
+					<Products products={shownProducts} orderByOption={orderByOption} />
+				)}
 
 				{width > lg && (
 					<Container>
 						<div className='mt-6 flex gap-14'>
 							{width > lg && <Categories />}
-							<Products />
+							{shownProducts && (
+								<Products
+									products={shownProducts}
+									orderByOption={orderByOption}
+								/>
+							)}
 						</div>
 					</Container>
 				)}
