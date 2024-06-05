9c3e31cab2ec9062eac9d147b347d96c720123d4
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-05 16:07:48 -0400

    Discounts display added

--- src/pages/Store/Product.jsx
+++ src/pages/Store/Product.jsx
@@ -2,7 +2,7 @@ import { BiHeart, BiPlus } from 'react-icons/bi'
 import { urlFor } from '../../lib/client'
 import { Link } from 'react-router-dom'
 
-function Product({ name, price, images: img, slug }) {
+function Product({ name, price, images: img, _id, discount, seasonDiscount }) {
 	return (
 		<div className='relative max-w-[360px] lg:max-w-sm'>
 			<div className='absolute right-0 top-0 flex justify-end px-6 pt-6'>
@@ -17,12 +17,30 @@ function Product({ name, price, images: img, slug }) {
 					/>
 				</div>
 			</div>
-			<Link to={`/store/${slug.current}`}>
+			<Link to={`/store/${_id}`}>
 				<img src={urlFor(img)} className='w-full' />
 			</Link>
-			<h3 className='absolute bottom-0 flex w-full items-end justify-between px-6 pb-6 text-lg font-medium'>
+			<h3 className='absolute bottom-0 flex w-full items-center justify-between px-6 pb-6 text-lg font-medium'>
 				<span>{name}</span>
-				<span>${price}</span>
+				<div className='flex flex-col'>
+					{!discount && !seasonDiscount && <span>${price}</span>}
+					{discount && (
+						<>
+							<span className='text-red-500 line-through'>${price}</span>
+							<span className='text-green-500'>
+								${price - price * (discount / 100)}(%{discount})
+							</span>
+						</>
+					)}
+					{seasonDiscount && (
+						<>
+							<span className='text-red-500 line-through'>${price}</span>
+							<span className='text-green-500'>
+								${price - price * (seasonDiscount / 100)}(%{seasonDiscount})
+							</span>
+						</>
+					)}
+				</div>
 			</h3>
 		</div>
 	)
