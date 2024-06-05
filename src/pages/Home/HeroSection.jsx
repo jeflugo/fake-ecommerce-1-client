aa0bd1513916882835d02bcb649f4e70ef300491
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-05 14:06:54 -0400

    Newest product functionality added

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,31 +1,50 @@
 import { Button } from '@material-tailwind/react'
 import { HERO_BANNERS } from '../../constants'
 import { useStateContext } from '../../context/StateContext'
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { client } from '../../lib/client'
 
 function HeroSection() {
 	const { SM, MD, LG } = HERO_BANNERS
 	const { md, lg, width } = useStateContext()
-	return (
-		<div className='relative lg:bg-gray-50 lg:py-6'>
-			{width < md && <img src={SM} className='w-full' />}
-			{width > md && width < lg && <img src={MD} className='w-full' />}
-			{width > lg && <img src={LG} className='ml-10 w-1/2' />}
+	const [newestProduct, setNewestProduct] = useState()
+	useEffect(() => {
+		const productsQuery = `*[_type=="product" && isNewest][0]{name, slug,price}`
 
-			<div className='absolute lg:right-6 lg:top-0 lg:flex lg:h-full lg:items-center lg-max:bottom-0 lg-max:w-full lg-max:pb-10'>
-				<div className='mx-auto w-9/12 max-w-[380px] lg:mx-0'>
-					<h1 className='mb-6 text-center text-4xl font-medium  lg:text-left'>
-						<span className='underline'>NEW</span> Air Force Max Storm
-					</h1>
-					<div className='flex justify-center gap-4 lg:justify-start'>
-						<Button size='lg'>Buy now</Button>
-						<div>
-							<p className='text-red-600 line-through'>$400</p>
-							<p className='text-green-600'>$200 (50 %off)</p>
+		client.fetch(productsQuery).then(data => setNewestProduct(data))
+	}, [])
+	return (
+		<>
+			{newestProduct && (
+				<div className='relative lg:bg-gray-50 lg:py-6'>
+					{width < md && <img src={SM} className='w-full' />}
+					{width > md && width < lg && <img src={MD} className='w-full' />}
+					{width > lg && <img src={LG} className='ml-10 w-1/2' />}
+
+					<div className='absolute lg:right-20 lg:top-0 lg:flex lg:h-full lg:items-center lg-max:bottom-0 lg-max:w-full lg-max:pb-10'>
+						<div className='mx-auto lg:mx-0'>
+							<h1 className='mx-auto mb-6 max-w-[300px] text-center  text-4xl font-medium lg:text-left'>
+								<span className='underline'>NEW</span> {newestProduct.name}
+							</h1>
+							<div className='flex justify-center gap-4 lg:justify-start'>
+								<Link to={`/store/${newestProduct.slug.current}`}>
+									<Button size='lg'>Buy now</Button>
+								</Link>
+								<div>
+									<p className='text-red-600 line-through'>
+										${newestProduct.price}
+									</p>
+									<p className='text-green-600'>
+										${newestProduct.price / 2} (50 %off)
+									</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</>
 	)
 }
 
