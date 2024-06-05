5f35bd6518b0020023dfb1c2242f6781d3be7ad5
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-05 16:08:10 -0400

    orderByOption parameter removed

--- src/pages/Store/Products.jsx
+++ src/pages/Store/Products.jsx
@@ -1,6 +1,6 @@
 import Product from './Product'
 
-function Products({ products, orderByOption }) {
+function Products({ products }) {
 	return (
 		<div className='grid w-full grid-cols-1 place-items-center gap-2 md:grid-cols-2 lg:grid-cols-3'>
 			{products && (
