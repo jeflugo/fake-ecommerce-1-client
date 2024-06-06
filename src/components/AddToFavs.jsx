59616b19f4e4ae77c338b8a5ef00de84f681eafa
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-05 21:06:26 -0400

    AddToFavs component added

---
+++ src/components/AddToFavs.jsx
@@ -0,0 +1,15 @@
+import { BiHeart } from 'react-icons/bi'
+import { useStateContext } from '../context/StateContext'
+
+export default function AddToFavs() {
+	const { addToFavs } = useStateContext()
+	return (
+		<BiHeart
+			size={30}
+			className='transition-all hover:scale-125 hover:fill-red-600 active:scale-100'
+			onClick={() => {
+				addToFavs()
+			}}
+		/>
+	)
+}
