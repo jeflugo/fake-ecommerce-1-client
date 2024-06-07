3cf7f0943d78c3f515db18323ad02adf559ccd3b
jeflugo <jefersoneduardoluego@gmail.com>
2024-06-06 23:02:55 -0400

    basic cart functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,88 @@
+import { AiOutlineClose } from 'react-icons/ai'
+import Container from './Container'
+import { useStateContext } from '../context/StateContext'
+import { urlFor } from '../lib/client'
+import { BiChevronDown, BiChevronUp } from 'react-icons/bi'
+import { useEffect } from 'react'
+
 function Cart() {
-	return <div>Cart</div>
+	const { cartProducts, toggleCart, showCart } = useStateContext()
+
+	useEffect(() => {
+		if (showCart) {
+			document.body.style.overflow = 'hidden'
+		} else {
+			document.body.style.overflow = 'auto'
+		}
+
+		return () => {
+			document.body.style.overflow = 'auto'
+		}
+	}, [showCart])
+	return (
+		<>
+			{showCart && (
+				<div
+					className={`absolute right-0 top-0 h-screen w-[100vw] bg-white md:w-[50vw] lg:w-[35vw]`}
+				>
+					<div className='py-8'>
+						<Container>
+							<div className='flex items-center justify-between '>
+								<h2 className='text-2xl'>Cart products</h2>
+								<button className='text-lg font-bold' onClick={toggleCart}>
+									<AiOutlineClose />
+								</button>
+							</div>
+							{cartProducts.length !== 0 ? (
+								<div className='hide-scrollbar mt-6 h-[75vh] overflow-y-auto'>
+									{cartProducts.map(
+										({ name, img, size, qty, totalPrice, _id }) => (
+											<div
+												key={_id}
+												className='mb-4 flex h-[90px] overflow-hidden rounded-md shadow-md'
+											>
+												<img
+													src={urlFor(img)}
+													alt={name}
+													className='h-full w-[25%]'
+												/>
+												<div className='flex w-[75%] items-center justify-between py-4 pl-4 pr-6'>
+													<div>
+														<h2 className='font-bold'>
+															{name} (#{size})
+														</h2>
+														<p>
+															Total:{' '}
+															<span className='font-medium text-green-600'>
+																${totalPrice}
+															</span>
+														</p>
+													</div>
+													<div className='flex flex-col items-center'>
+														<button className='text-3xl'>
+															<BiChevronUp />
+														</button>
+														<p className='font-bold'>{qty}</p>
+														<button className='text-3xl'>
+															<BiChevronDown />
+														</button>
+													</div>
+												</div>
+											</div>
+										),
+									)}
+								</div>
+							) : (
+								<div className='mt-20 text-center text-lg'>
+									There&apos;s nothing in the cart
+								</div>
+							)}
+						</Container>
+					</div>
+				</div>
+			)}
+		</>
+	)
 }
 
 export default Cart
--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,14 +1,28 @@
 import { Badge, Tooltip } from '@material-tailwind/react'
 import { BiCart } from 'react-icons/bi'
+import { useStateContext } from '../context/StateContext'
 
 export default function CartButton() {
+	const { showCart, toggleCart, cartProducts } = useStateContext()
 	return (
-		<div className='fixed right-0 top-[20%] translate-x-1 transition-all hover:-translate-x-0'>
-			<Badge content={2} placement='top-start' overlap='circular'>
-				<Tooltip content='Check Cart' placement='top-start'>
-					<button className='rounded-bl-full rounded-tl-full bg-black px-2 py-1 text-3xl text-white shadow-xl'>
+		<div
+			className={`fixed right-0 top-[20%] translate-x-1 transition-all hover:translate-x-0`}
+			onClick={toggleCart}
+		>
+			<Badge
+				content={cartProducts.length}
+				placement='top-start'
+				overlap='circular'
+				className={`${cartProducts.length === 0 ? 'hidden' : 'absolute'}`}
+			>
+				<Tooltip
+					content='CheckCart'
+					placement='top-start'
+					className={showCart ? 'hidden' : 'inherit'}
+				>
+					<div className='rounded-bl-full rounded-tl-full bg-black px-2 py-1 text-3xl text-white shadow-xl'>
 						<BiCart />
-					</button>
+					</div>
 				</Tooltip>
 			</Badge>
 		</div>
